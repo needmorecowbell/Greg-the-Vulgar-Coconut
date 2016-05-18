b62ce5911b27bc5a1da0291cf1ce9a3f4e316d58
Adam Musciano <amusciano@gmail.com>
2016-05-18 17:44:52 -0400

    First commit of greg the vulgar coconut

---
+++ CirclePlatform2d/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	public float speed;
+
+	void Start(){
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector2 movement = new Vector2 (moveHorizontal,moveVertical);
+		rb.AddForce (movement*speed);
+	}
+}
