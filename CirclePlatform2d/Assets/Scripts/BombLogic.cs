67a11148791f51777da676667d4ca06c19135d44
Adam Musciano <amusciano@gmail.com>
2016-05-20 09:51:39 -0400

    Added Bomb sounds and logic, various other sounds

--- CirclePlatform2d/Assets/Scripts/BombLogic.cs
+++ CirclePlatform2d/Assets/Scripts/BombLogic.cs
@@ -5,10 +5,13 @@ public class BombLogic : MonoBehaviour {
 	private float timeEnd=0;
 	private bool isAffected = false;
 	public float duration = 5f;
-	private bool didOnce= false;
+	private bool didOnce= false;//this is bad
+	private bool didOnce2 = false; //this is so bad. I should be ashamed of myself
+	public AudioClip[] explosionClips;
+	private AudioSource audio;
 	// Use this for initialization
 	void Start () {
-
+		audio = GetComponent<AudioSource> ();
 	}
 
 	// Update is called once per frame
@@ -23,13 +26,27 @@ public class BombLogic : MonoBehaviour {
 
 			if (Time.time <= timeEnd) {
 				Debug.Log (timeEnd-Time.time);
+				if ((int)(Time.time*10) % 2==0) { //Tenth of a second
+					gameObject.GetComponent<SpriteRenderer> ().color = Color.red;
+				} else {
+					gameObject.GetComponent<SpriteRenderer> ().color = Color.white;
+				}
 				//do graphical animation
 
 			}
 			if (Time.time >= timeEnd) {
 				Debug.Log ("BOOM!");
-				isAffected = false;
-				Destroy (gameObject);
+
+				if (!didOnce2) {
+					audio.PlayOneShot(explosionClips[Random.Range(0,explosionClips.Length-1)]);
+					didOnce2 = true;
+				}
+
+				if(!audio.isPlaying){
+					isAffected = false;
+					Destroy (gameObject);
+				}
+
 			}
 
 		}
@@ -38,7 +55,7 @@ public class BombLogic : MonoBehaviour {
 	}
 	void OnCollisionEnter2D(Collision2D col){
 		if (col.gameObject.tag == "Player") {
-
+			
 			isAffected=true;
 		}
 	}
