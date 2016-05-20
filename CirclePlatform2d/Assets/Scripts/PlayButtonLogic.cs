8d2d6916ae805746728bac34fa3d85906bdf819f
Adam Musciano <amusciano@gmail.com>
2016-05-20 14:43:31 -0400

    Added menu scene, and sky movement.

---
+++ CirclePlatform2d/Assets/Scripts/PlayButtonLogic.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayButtonLogic : MonoBehaviour {
+	Button btnPlay;
+
+	void Start(){
+		btnPlay = GetComponent<Button> ();
+		btnPlay.onClick.AddListener (() => changeScene ());
+
+	}
+
+	void changeScene(){
+		
+		SceneManager.LoadScene ("GregTheVulgarCoconut");
+	}
+}
---
+++ CirclePlatform2d/Assets/Scripts/SkyMove.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkyMove : MonoBehaviour {
+	
+	private Vector3 startPos;
+	public float size=5;
+	public float scrollSpeed;
+	private bool isLeft = true;
+
+
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (isLeft) {
+			
+			float newPos = Mathf.Repeat (Time.time * scrollSpeed, size);
+			transform.position = startPos + Vector3.left * newPos;
+			//isLeft = false;
+			Debug.Log (newPos);
+
+		} else {
+			float newPos = Mathf.Repeat (Time.time * scrollSpeed, size);
+			transform.position = startPos + Vector3.right * newPos;
+			//if(transform.position
+			//isLeft = true;
+
+		}
+
+	}
+}
