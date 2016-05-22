06e718047ecfef4090ebd4135088bd3405829c17
Adam Musciano <amusciano@gmail.com>
2016-05-22 12:01:36 -0400

    Added level1 and level movement

---
+++ CirclePlatform2d/Assets/Scripts/PortalBehavior.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class PortalBehavior : MonoBehaviour {
+	private int scene;
+	public bool enabled= false;
+	// Use this for initialization
+	void Start () {
+
+		scene = SceneManager.GetActiveScene ().buildIndex;
+
+	}
+
+	void sceneStart(){
+		SceneManager.LoadScene (0);
+	}
+	
+	void OnCollisionEnter2D(Collision2D col){
+		if (col.gameObject.tag == "Player") {
+			SceneManager.LoadScene (scene+1);
+		}
+	}
+}
